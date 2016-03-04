6115b1e4b7c7b810889e7b1b570f746a430d3b29
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-03 19:04:43 -0500

    pass dest to conflicts

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,9 @@ var gulp = require('gulp');
 var conflicts = require('./conflicts');
 
 gulp.task('conflicts', function(cb) {
+  var dest = 'vendor/yeoman-generator';
   gulp.src('vendor/fixtures/*')
-    .pipe(conflicts())
-    .pipe(gulp.dest('vendor/yeoman-generator'))
+    .pipe(conflicts(dest))
+    .pipe(gulp.dest(dest))
     .on('end', cb);
 });
-
-gulp.task('default', ['conflicts']);
