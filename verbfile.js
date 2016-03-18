e3da2902f8b4333ed8c5049d413cea6446738d0a
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-17 20:00:01 -0400

    clean up example

--- verbfile.js
+++ verbfile.js
@@ -1,19 +1,26 @@
 'use strict';
 
 var path = require('path');
-var conflicts = require('./');
 var cwd = path.join.bind(path, __dirname, 'test/fixtures');
+var conflicts = require('./');
 
 /**
- * Example usage
+ * Example usage with verb
+ *
+ * ```sh
+ * $ verb conflicts
+ * ```
  */
 
 module.exports = function(verb) {
+  verb.extendWith('verb-readme-generator');
   verb.use(conflicts());
 
-  verb.task('default', function() {
+  verb.task('conflicts', function() {
     return verb.src(cwd('*.txt'))
       .pipe(verb.conflicts(cwd('dist')))
       .pipe(verb.dest(cwd('dist')));
   });
+
+  verb.task('default', ['readme']);
 };
